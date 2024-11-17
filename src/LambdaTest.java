43120e6ec26b2541c05d740009b6ae306ed865e1
Serhii Ivanov <0636278046i@gmail.com>
2024-11-17 23:43:48 +0200

    HW 11

--- src/LambdaTest.java
+++ src/LambdaTest.java
@@ -15,8 +15,9 @@ public class LambdaTest {
                 {-1.0, 10.0, 10.0},
                 {0.0, 0.0, 0.0},
                 {-10.0, NaN, NaN},
-                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+                {POSITIVE_INFINITY, 1e32, POSITIVE_INFINITY},
+                {-100.0, -200.0, -100.0},
+                {Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY}
         };
     }
 
@@ -25,8 +26,9 @@ public class LambdaTest {
         return new Object[][]{
                 {25.0, 5.0},
                 {-1.0, NaN},
-                {POSITIVE_INFINITY, POSITIVE_INFINITY}
-                // TODO add 2 more test data here
+                {POSITIVE_INFINITY, POSITIVE_INFINITY},
+                {0.0, 0.0},
+                {49.0, 7.0}
         };
     }
 
